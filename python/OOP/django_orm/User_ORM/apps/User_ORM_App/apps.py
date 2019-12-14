37bb459f9adbef9f1b3cd15e9e15636346d63f5e
Jaden Scott <30578904+JadenMScott@users.noreply.github.com>
2019-12-14 14:12:07 -0600

    Add files via upload

---
+++ python/OOP/django_orm/User_ORM/apps/User_ORM_App/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UserOrmAppConfig(AppConfig):
+    name = 'User_ORM_App'
---
+++ python/OOP/django_orm/books_author_temp/apps/books_authors_temp_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BooksAuthorsTempAppConfig(AppConfig):
+    name = 'books_authors_temp_app'
